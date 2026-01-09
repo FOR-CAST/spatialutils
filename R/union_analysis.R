2f32f79aa7216b350422429b17a7663f157024d9
Alex Chubaty <achubaty@for-cast.ca>
2026-01-09 13:38:38 -0700

    speed up union_analysis calculations
    prefilter and subset intersecting polygons

--- R/union_analysis.R
+++ R/union_analysis.R
@@ -33,21 +33,19 @@
 #'   ggplot2::geom_sf(data = y, fill = "darkgreen", alpha = 0.5)
 #'
 #' ## uses attribute values from x for intersections
-#' i <- sf::st_intersection(x, y) |>
+#' xy <- sf::st_intersection(x, y) |>
 #'   sf::st_make_valid() |>
 #'   dplyr::select(-dplyr::ends_with(".1"))
 #'
 #' dx <- sf::st_difference(x, sf::st_union(y))
 #' dy <- sf::st_difference(y, sf::st_union(x))
 #'
-#' u <- rbind(i, dx, dy) |>
-#'   dplyr::summarise()
-#' identical(u, st_union_analysis(x, y))
+#' u <- st_union_analysis(x, y)
 #'
 #' ggplot2::ggplot() +
 #'   ggplot2::geom_sf(data = dx, fill = "lightblue", alpha = 0.25) +
 #'   ggplot2::geom_sf(data = dy, fill = "darkred", alpha = 0.25) +
-#'   ggplot2::geom_sf(data = i, fill = "purple", alpha = 0.25) +
+#'   ggplot2::geom_sf(data = xy, fill = "purple", alpha = 0.25) +
 #'   ggplot2::geom_sf(data = u, fill = NA, col = "black", alpha = 0.25, linewidth = 1.2)
 #'
 #' @export
@@ -55,27 +53,61 @@ st_union_analysis <- function(x, y, union_by = NULL) {
   x <- sf::st_set_geometry(x, "geometry")
   y <- sf::st_set_geometry(y, "geometry")
 
-  i <- sf::st_intersection(x, y) |> sf::st_make_valid() |> dplyr::select(-dplyr::ends_with(".1"))
+  ## pre-filter the intersecting geometries to speed up calcs
+  intersects_idx <- sf::st_intersects(x, y)
+  intersects_idy <- unique(unlist(intersects_idx))
 
-  dx <- sf::st_difference(x, sf::st_union(y))
-  dy <- sf::st_difference(y, sf::st_union(x))
+  ## calculate intersections for intersecting polygons
+  intersect_list <- list()
+  k <- 1
+  for (i in seq_len(nrow(x))) {
+    which_y_intersects <- intersects_idx[[i]]
+
+    if (length(which_y_intersects) > 0) {
+      for (j in which_y_intersects) {
+        intersect_list[[k]] <- sf::st_intersection(x[i, ], y[j, ])
+        k <- k + 1
+      }
+    }
+  }
+
+  ## keep attribute values from x (not y)
+  intersected <- dplyr::bind_rows(intersect_list) |>
+    sf::st_make_valid() |>
+    dplyr::select(-dplyr::ends_with(".1"))
+
+  ## calculate differences for intersecting polygons
+  x_intersects <- lengths(intersects_idx) > 0
+  subset_x <- x[x_intersects, ]
+  subset_y <- y[intersects_idy, ]
+
+  dx <- sf::st_difference(subset_x, sf::st_union(subset_y))
+  dy <- sf::st_difference(subset_y, sf::st_union(subset_x))
+
+  ## deal with non-intersecting polygons
+  nonintersected_x <- x[!x_intersects, ]
+  nonintersected_y <- y[!(seq_len(nrow(y)) %in% intersects_idy), ]
 
   names_diff_x <- setdiff(names(x), names(y))
   names_diff_y <- setdiff(names(y), names(x))
 
   if (length(names_diff_y) > 0) {
     for (col_name in seq_along(names_diff_y)) {
-      dx <- dplyr::mutate(dx, {{ col_name }} := NA_character_, .before = "geometry")
+      dx <- dx |> dplyr::mutate({{ col_name }} := NA_character_, .before = "geometry")
+      nonintersected_x <- nonintersected_x |>
+        dplyr::mutate({{ col_name }} := NA_character_, .before = "geometry")
     }
   }
 
   if (length(names_diff_x) > 0) {
     for (col_name in seq_along(names_diff_x)) {
-      dy <- dplyr::mutate(dy, {{ names_diff_x }} := NA_character_, .before = "geometry")
+      dy <- dy |> dplyr::mutate({{ col_name }} := NA_character_, .before = "geometry")
+      nonintersected_y <- nonintersected_y |>
+        dplyr::mutate({{ col_name }} := NA_character_, .before = "geometry")
     }
   }
 
-  u <- rbind(i, dx, dy)
+  u <- rbind(intersected, dx, dy, nonintersected_x, nonintersected_y)
 
   if (is.null(union_by)) {
     u <- u |> dplyr::summarise()
